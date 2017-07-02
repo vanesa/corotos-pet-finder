e56e7ad73529321b2abf1e64a62aad9a755c60d6
Vanesa <vanesa@gmx.net>
2017-07-01 19:21:37 -0700

    add redis for subscription

--- sms_handler.py
+++ sms_handler.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 try:
     import secrets
+import redis
 
 twillio_account_sid = secrets.TWILIO_ACCOUNT_SID
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
 
@@ -10,6 +12,8 @@ app = Flask(__name__)
 def handle_sms():
 	user = request.form['From']
 	pet = request.form['Body'].strip().lower()
+
+	redis_client.sadd(pet, user.encode('utf-8'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
