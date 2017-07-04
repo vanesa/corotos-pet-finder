c953a8f76fc3f4d7ea99ccf3644e6c0c158a79d5
Vanesa <vanesa@gmx.net>
2017-07-03 23:50:56 -0700

    add notification to users

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import redis
+import time
+from twilio.rest import Client
+from secrets import twilio_account_sid, token, my_number
+
 
 URL = 'https://www.corotos.com.do/republica_dominicana/animales_y_mascotas-en_venta'
 PET_NUM_NDX = 0
@@ -16,8 +21,22 @@ def get_pets():
         if links:
             pets.append(links[PET_NUM_NDX])
 
+    return pets
+
+client = Client(twilio_account_sid, token)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+ 
+def message(recipient, body):
+    message = client.messages.create(to=recipient, from_=my_number, body=body)
+ 
+ 
+if __name__ == '__main__':
+    pets = get_pets()
     for pet in pets:
-    	if 'pitbull' in pet.lower():
-    		found_pet = True 
-    
-    return 'Found Pet: ', found_pet
+        if 'johnson' or 'king charles' not in pet.lower():
+            continue
+ 
+        to_notify = redis_client.smembers(pet)
+        for user in to_notify:
+            message(user.decode('utf-8'), body="Good news! We found a {pet}.")
+            redis_client.srem(pet, user)
--- sms_handler.py
+++ sms_handler.py
@@ -1,31 +1,34 @@
 from flask import Flask, request
-try:
-    import secrets
 import redis
+from secrets import twilio_account_sid, token, my_number
 from twilio.twiml.messaging_response import MessagingResponse
 
-twillio_account_sid = secrets.TWILIO_ACCOUNT_SID
-my_number = secrets.MY_NUMBER
-requested_pets = {'cavalier', 'king charles spaniel'}
-
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+requested_pets = {'cavalier', 'king charles spaniel', 'johnson'}
 
 app = Flask(__name__)
 
 def respond(user, body):
     response = MessagingResponse()
     response.message(body=body)
+    
     return str(response)
 
-@app.route('/sms', method=['POST'])
+@app.route('/sms', methods=['POST'])
 def handle_sms():
+
+	redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+	if request.form['AccountSid'] != twilio_account_sid:
+		return '', 400
+
 	user = request.form['From']
 	pet = request.form['Body'].strip().lower()
+	
 	if pet not in requested_pets:
-        return respond(user, body="Hm, that doesn't look like a valid pet. Try something like 'Cavalier'.")
- 
-    redis_client.sadd(pet, user.encode('utf-8'))
-        return respond(user, body=f"Sweet action. We'll let you know when there are {pet}'s available.")
+		return respond(user, body="Hm, that doesn't look like a valid pet. Try something like 'Johnson'.")
+
+	redis_client.sadd(pet, user.encode('utf-8'))
+	return respond(user, body="Sweet action. We'll let you know when there are {pet}'s available.")
 
 
 if __name__ == '__main__':
